63b20c47a9eb1d9c140b0ccbf5d73f13c5c3cace
Alexandre Hajjar <alexandre.hajjar@gmail.com>
2013-01-18 11:00:26 +0100

    removed reference to OrigCtrl

--- angular_app/app/js/app.js
+++ angular_app/app/js/app.js
@@ -5,7 +5,6 @@
 angular.module('timeseriesApp', ['timeseriesApp.filters', 'timeseriesApp.services', 'timeseriesApp.directives']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/graph', {templateUrl: 'partials/graph.html', controller: GraphCtrl});
-    $routeProvider.when('/orig', {templateUrl: 'partials/orig.html', controller: OrigCtrl});
     $routeProvider.otherwise({redirectTo: '/graph'});
   }]);
 
